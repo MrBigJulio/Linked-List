dd0230d158c5d88f8ac578b706f7db4d6c35ced6
Vadim Savelyev <sv48136@zut.edu.pl>
2021-10-20 17:24:41 +0200

    Dodaj pliki projektów.

---
+++ Linked List/LinkedList.h
@@ -0,0 +1,34 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+template <class T>
+class Lista {
+private:
+	struct Wezel {
+		T dane;
+		struct Wezel* nastepny;
+		struct Wezel* poprzedni;
+	};
+
+	Wezel* glowa;
+	Wezel* ogon;
+
+public:
+	Lista() {
+		glowa = nullptr
+	}
+
+	~Lista();
+
+	dodajNaKoncu();
+	dodajNaPoczatku();
+	usunPierwszy();
+	usunOstatni();
+	wypisz();
+	zmien();
+	wyszukaj();
+
+};
+
+
+#endif // !LINKEDLIST.H
---
+++ Linked List/main.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+#include "LinkedList.h"
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
