e559505df0d346842f714229aad53a05b95443c6
Vadim Savelyev <mrbigjulio@gmail.com>
2021-10-21 18:44:33 +0200

    1 test done

--- Linked List/LinkedList.h
+++ Linked List/LinkedList.h
@@ -1,5 +1,61 @@
 #ifndef LINKEDLIST_H
 #define LINKEDLIST_H
 
+class Object {
+public:
+	int data1;
+	char data2;
+	Object* next;
+	Object* prev;
+
+	Object() {
+		next = nullptr;
+		prev = nullptr;
+	}
+};
+
+template <class T>
+class List {
+private:
+	Object* head;
+	Object* tail;
+	int size;
+
+public:
+	List() {
+		head = nullptr;
+		tail = nullptr;
+		size = 0;
+	};
+
+	~List();
+
+	void addToEnd(T);
+	void addToStart(T);
+	Object print(int, Object);
+};
+
+template <class T>
+void List<T>::addToEnd(T objectData) {
+	Object* newObject;
+	Object* objectPrt;
+
+	newObject = new Object;
+	newObject->data1 = objectData.data1;
+	newObject->data2 = objectData.data2;
+	size++;
+
+	if (!head) {
+		head = newObject;
+		tail = newObject;		
+	}
+	else
+	{
+		objectPrt = tail;
+		newObject->prev = objectPrt;
+		objectPrt->next = newObject;
+		tail = newObject;
+	}
+}
 
 #endif // !LINKEDLIST.H
--- Linked List/main.cpp
+++ Linked List/main.cpp
@@ -1,9 +1,20 @@
 ﻿#include <iostream>
 #include "LinkedList.h"
-#include <vector>
 using namespace std;
 
 int main()
 {
-	
+	List<Object>* list = new List<Object>();
+	Object* newObject = new Object();
+	newObject->data1 = 123;
+	newObject->data2 = 'a';
+	list->addToEnd(*newObject);
+	Object* newObject2 = new Object();
+	newObject2->data1 = 312;
+	newObject2->data2 = 'b';
+	list->addToEnd(*newObject2);
+	Object* newObject3 = new Object();
+	newObject3->data1 = 231;
+	newObject3->data2 = 'c';
+	list->addToEnd(*newObject3);
 };
